c042ef1b4a2e352d13d22f0ef48c68b79152e0ed
Juanita\Lukas Roberson <lukeroberson24@yahoo.com>
2021-08-09 15:28:40 -0400

    adding questions

---
+++ assets/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+{
+title: "How many Call of Duty games have been released?",
+choices: ["19", "27", "24", "22"],
+answer: "Answer was 24"
+},
+{
+title: "When was the first Call of Duty game released?",
+choices: [""],
+answer: ""
+},
+{
+title: "Who owns the Call of Duty IP",
+choices: ["Activision", "Square Enix", "Electronic Arts(EA)", "Bandai Namco"],
+answer: "Answer is Activision"
+},
+{
+title: "When was the first Call of Duty World League tournament held",
+choices: [""],
+answer: ""
+},
+{
+title: "How many Call of Duty Leage teams are there currently",
+choices: [""],
+answer: ""
+}
+]
---
+++ assets/scores.js
@@ -0,0 +1,2 @@
+var printHighschores
+// clear highschores
